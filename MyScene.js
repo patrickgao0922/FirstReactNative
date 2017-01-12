df8eeb3190a96b96e8e256636c04490f373c1a83
Patrick Gao <patrickgao0922@gmail.com>
2017-01-12 22:16:30 +1100

    Create class body

--- MyScene.js
+++ MyScene.js
@@ -1,1 +1,19 @@
 import React, {Component} from 'react'
+import { View, Text, Navigation } from "react-native"
+
+export default class MyScene extends Component {
+    static get defaultProps() {
+        return {
+            title: 'MyScene'
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Hi! My name is {this.props.title}</Text>
+            </View>
+        )
+        
+    }
+}
